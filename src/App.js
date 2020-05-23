77c8b3b67653d137c5405c1fe06f858b0c44964a
brandon quezada <brandon.cypaso@gmail.com>
2020-05-23 10:33:56 -0500

    changed around the buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import { openSync } from 'fs';
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const op = ['/', '*', '-', '+', '='];
+const op = ['/', '×', '-', '+', '='];
 
 function App() {
 	return (
@@ -12,11 +12,13 @@ function App() {
 				1500
 			</div>
 			<div className='nums-container'>
+				<button className='big-h light-grey ac'>AC</button>
 				{nums.map((num) => (
 					<button className={`dark-grey ${num === 0 && 'big-h'}`} key={num}>
 						{num}
 					</button>
 				))}
+				<button className='light-grey'>.</button>
 			</div>
 			<div className='ops-container'>
 				{op.map((op) => (
