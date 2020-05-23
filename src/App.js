e021270f2d83836027aa253080b4dc2fde95f469
brandon quezada <brandon.cypaso@gmail.com>
2020-05-23 11:22:27 -0500

    made it a class App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import './App.css';
-// import { openSync } from 'fs';
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
 const op = ['/', '×', '-', '+', '='];
 
-function App() {
-	return (
-		<div className='calculator'>
-			<div id='display' className='display'>
-				1500
+class App extends React.Component {
+	render() {
+		return (
+			<div className='calculator'>
+				<div id='display' className='display'>
+					1500
+				</div>
+				<div className='nums-container'>
+					<button className='big-h light-grey ac'>AC</button>
+					{nums.map((num) => (
+						<button className={`dark-grey ${num === 0 && 'big-h'}`} key={num}>
+							{num}
+						</button>
+					))}
+					<button className='light-grey'>.</button>
+				</div>
+				<div className='ops-container'>
+					{op.map((op) => (
+						<button className='orange' key={op}>
+							{op}
+						</button>
+					))}
+				</div>
 			</div>
-			<div className='nums-container'>
-				<button className='big-h light-grey ac'>AC</button>
-				{nums.map((num) => (
-					<button className={`dark-grey ${num === 0 && 'big-h'}`} key={num}>
-						{num}
-					</button>
-				))}
-				<button className='light-grey'>.</button>
-			</div>
-			<div className='ops-container'>
-				{op.map((op) => (
-					<button className='orange' key={op}>
-						{op}
-					</button>
-				))}
-			</div>
-		</div>
-	);
+		);
+	}
 }
 
 export default App;
