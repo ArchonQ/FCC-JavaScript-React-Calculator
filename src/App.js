8465e58a11771aaaf9a2b35f21da70a278c58b31
brandon quezada <brandon.cypaso@gmail.com>
2020-05-26 18:22:04 -0500

    trying some logic recoding

--- src/App.js
+++ src/App.js
@@ -2,74 +2,116 @@ import React from 'react';
 import './App.css';
 
 const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-const op = ['/', '×', '-', '+', '='];
+const op = ['/', '*', '-', '+', '='];
 
 class App extends React.Component {
 	state = {
 		lastPressed: undefined,
 		currentNumber: '0',
-		prevNumber: undefined,
+		calc: undefined,
 		operation: undefined,
 	};
 
 	handleClick = (e) => {
-		const { currentNumber, prevNumber, operation } = this.state;
+		const { calc, currentNumber } = this.state;
 		const { innerText } = e.target;
 
-		if (!Number.isNaN(Number(innerText))) {
-			if (currentNumber === '0') {
-				this.setState({
-					currentNumber: innerText,
-				});
-			} else {
-				this.setState({
-					currentNumber: currentNumber + innerText,
-				});
-			}
-			return;
-		}
+		this.setState({
+			calc: currentNumber + innerText,
+			currentNumber: currentNumber + innerText,
+			lastPressed: innerText,
+		});
 
 		switch (innerText) {
 			case 'AC': {
 				this.setState({
 					currentNumber: '0',
-					prevNumber: undefined,
-					operation: undefined,
+					calc: undefined,
 				});
-				break;
+				return;
 			}
-			case '.': {
-				if (!currentNumber.includes('.')) {
-					this.setState({
-						currentNumber: currentNumber + innerText,
-					});
-				}
-				break;
-			}
-			default: {
-				if (!operation) {
-					this.setState({
-						operation: innerText,
-						prevNumber: currentNumber,
-						currentNumber: '',
-					});
-				} else {
-					const evalued = eval(`${prevNumber} ${operation} ${currentNumber}`);
-					this.setState({
-						operation: innerText,
-						prevNumber: evalued,
-						currentNumber: innerText === '=' ? evalued : '0',
-					});
-				}
+			case '=': {
+				const evaluated = eval(calc);
+				this.setState({
+					currentNumber: evaluated,
+					calc: evaluated,
+				});
 			}
 		}
+
+		// const { currentNumber, calc, operation, lastPressed } = this.state;
+		// const { innerText } = e.target;
+		// this.setState({
+		// 	lastPressed: innerText,
+		// });
+		// if (!Number.isNaN(Number(innerText))) {
+		// 	if (currentNumber === '0') {
+		// 		this.setState({
+		// 			currentNumber: innerText,
+		// 		});
+		// 	} else if (openSync.includes(lastPressed)) {
+		// 		this.setState({
+		// 			currentNumber: innerText,
+		// 		});
+		// 	} else {
+		// 		this.setState({
+		// 			currentNumber: currentNumber + innerText,
+		// 		});
+		// 	}
+		// 	return;
+		// }
+		// switch (innerText) {
+		// 	case 'AC': {
+		// 		this.setState({
+		// 			currentNumber: '0',
+		// 			calc: undefined,
+		// 			operation: undefined,
+		// 		});
+		// 		break;
+		// 	}
+		// 	case '.': {
+		// 		if (!currentNumber.includes('.')) {
+		// 			this.setState({
+		// 				currentNumber: currentNumber + innerText,
+		// 			});
+		// 		}
+		// 		break;
+		// 	}
+		// 	default: {
+		// 		if (!operation) {
+		// 			this.setState({
+		// 				operation: innerText,
+		// 				calc: currentNumber,
+		// 				currentNumber: '',
+		// 			});
+		// 		} else if (innerText === '=') {
+		// 			const evaluated = eval(`${calc} ${operation} ${currentNumber}`);
+		// 			this.setState({
+		// 				operation: undefined,
+		// 				calc: evaluated,
+		// 				currentNumber: evaluated,
+		// 			});
+		// 		} else {
+		// 			const evaluated = eval(`${calc} ${operation} ${currentNumber}`);
+		// 			this.setState({
+		// 				operation: innerText,
+		// 				calc: evaluated,
+		// 				currentNumber: evaluated,
+		// 			});
+		// 		}
+		// 	}
+		// }
 	};
 
 	render() {
-		const { currentNumber } = this.state;
+		const { currentNumber, calc } = this.state;
 		return (
 			<div className='calculator'>
+				<p style={{ position: 'absolute', top: 0 }}>
+					{JSON.stringify(this.state, null, 2)}
+				</p>
 				<div id='display' className='display'>
+					<small>{calc}</small>
 					{currentNumber}
 				</div>
 				<div className='nums-container'>
